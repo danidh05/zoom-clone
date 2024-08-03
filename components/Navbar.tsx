974259024e3e04d488eb944ec994db5cc872ee23
danidh05 <150284540+danidh05@users.noreply.github.com>
2024-08-03 22:17:23 +0300

    finished reusable cards component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
     return (
@@ -21,7 +22,9 @@ const Navbar = () => {
                 </p>
             </Link>
             <div className="flex-between gap-5">
-                {/* Clerk User Managements */}
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
 
                 <MobileNav />
             </div>
